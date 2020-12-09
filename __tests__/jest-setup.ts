81940cd9af35af25ff9b19df70dbeaf9ca3e7b09
hmartiins <hmartins224@gmail.com>
2020-12-09 12:37:47 -0300

    initialize overnightJS and conluding the second stage of TDD

--- __tests__/jest-setup.ts
+++ __tests__/jest-setup.ts
@@ -1,11 +1,10 @@
 import { SetupServer } from '@src/server';
 import supertest from 'supertest';
 
-let server: SetupServer;
 beforeAll(async () => {
-  server = new SetupServer();
-  await server.init();
+  const server = new SetupServer();
+  server.init();
   global.testRequest = supertest(server.getApp());
 });
 
-afterAll(async () => await server.close());
+// afterAll(async () => await server.close());
