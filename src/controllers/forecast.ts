20f5d5acd91e904b1e23bff9c3cb70ebdc03886e
hmartiins <hmartins224@gmail.com>
2020-12-20 11:35:10 -0300

    Making the Forecast test break

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@overnightjs/core';
 import { Request, Response } from 'express';
 
-@Controller('forecast')
+@Controller('Forecast')
 export class ForecastController {
   @Get('')
   public getForecastForLoggedUser(_: Request, response: Response) : void {
