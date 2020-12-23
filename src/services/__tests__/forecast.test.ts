8a79dff77d6c0e72039671b2b85d72599dfad303
Henrique Martins <hmartins224@gmail.com>
2020-12-22 21:54:30 -0300

    performing internal error handling

--- src/services/__tests__/forecast.test.ts
+++ src/services/__tests__/forecast.test.ts
@@ -1,6 +1,6 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import stormGlassNormalizedResponseFixture from '../../clients/__tests__/fixtures/stormglass_normalized_response_3_hours.json';
-import { Forecast, Beach, BeachPosition } from '../forecast';
+import { Forecast, Beach, BeachPosition, ForecastProcessingInternalError } from '../forecast';
 
 jest.mock('@src/clients/stormGlass');
 
@@ -94,5 +94,26 @@ describe('Forecast Service', () => {
     const response = await forecast.processForecastForBeaches([]);
 
     expect(response).toEqual([]);
+  });
+  
+  it('should throw internal processing error when something goes wrong during the rating proccess', async() => {
+    const beaches: Beach[] = [
+      {
+        lat: -33.792726,
+        lng: 151.289824,
+        name: 'Manly',
+        position: BeachPosition.E,
+        user: 'some-id'
+      },
+    ];
+
+    mockedStormGlassService.fetchPoints.mockRejectedValue(
+      'Error fetching data'
+    );
+    
+    const forecast = new Forecast(mockedStormGlassService);
+    await expect(forecast.processForecastForBeaches(beaches)).rejects.toThrow(
+      ForecastProcessingInternalError
+    );
   });
 });
--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -1,5 +1,5 @@
 import { ForecastPoint, StormGlass } from '@src/clients/stormGlass';
-import { time } from 'console';
+import { InternalError } from '@src/utils/errors/internal-error';
 
 export enum BeachPosition {
   S = 'S',
@@ -23,6 +23,12 @@ export interface TimeForecast {
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
+export class ForecastProcessingInternalError extends InternalError {
+  constructor(message: string) {
+    super(`Unxpected error during the forecast processing ${message}`);
+  }
+}
+
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
@@ -30,21 +36,25 @@ export class Forecast {
     beaches: Beach[]
   ): Promise<TimeForecast[]> {
     const pointsWithCorrectSources: BeachForecast[] = [];
-    for(const beach of beaches) {
-      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
-      const enrichedBeachData = points.map((e) => ({
-        ...{
-          lat: beach.lat,
-          lng: beach.lng,
-          name: beach.name,
-          position: beach.position,
-          rating: 1
-        },
-        ...e,
-      }));
-      pointsWithCorrectSources.push(... enrichedBeachData);
+    try{
+      for(const beach of beaches) {
+        const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+        const enrichedBeachData = points.map((e) => ({
+          ...{
+            lat: beach.lat,
+            lng: beach.lng,
+            name: beach.name,
+            position: beach.position,
+            rating: 1
+          },
+          ...e,
+        }));
+        pointsWithCorrectSources.push(... enrichedBeachData);
+      }
+      return this.mapForecastByTime(pointsWithCorrectSources);
+    } catch(err) {
+      throw new ForecastProcessingInternalError(err.message);
     }
-    return this.mapForecastByTime(pointsWithCorrectSources);
   }
 
   private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
